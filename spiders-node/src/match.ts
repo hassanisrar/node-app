ddfa3cf7ff73fe740d6b4938a9fd7abe9d9a4bd8
Hassan <hassanisrar@hotmail.com>
2019-12-22 11:48:32 -0600

    my changes

---
+++ spiders-node/src/match.ts
@@ -0,0 +1,12 @@
+import 'source-map-support/register';
+
+export const score: any = async (event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Launchers Go.....!',
+      name: 'Hassan',
+      input: event,
+    }, null, 2),
+  };
+}
